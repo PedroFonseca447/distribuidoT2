e21f670bd865b79cdb937f86f527c22b7c49eb53
Pedro B B Fonseca <pedrofonseca447@gmail.com>
2022-10-18 18:44:23 +0000

    arrumando caixa

--- ProcessoCaixa.java
+++ ProcessoCaixa.java
@@ -2,60 +2,79 @@
     // Processo caixa
     // garante operações de saque , deposito e consulta de saldo
     // sendo nao idempotentes saque e deposito
-    import java.rmi.Naming;
-    import java.util.Scanner;
+    import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.Scanner;
 
 public class ProcessoCaixa {
-    public static void main(String args[]){
+    public static void main(String args[]) throws MalformedURLException, RemoteException, NotBoundException{
         Scanner in = new Scanner(System.in);
-        //while
-        try {
+        System.out.println("digite 1 para iniciar");
+         int escolha=Integer.parseInt(in.nextLine());
+         while(escolha==1) {
             //Procura pelo servico da calculadora no IP e porta definidos
             Banco c = (Banco) Naming.lookup("rmi://localhost:1099/CalcService");
    
             System.out.println("1 - saque");
             System.out.println("2 - deposito");
             System.out.println("3 - consulta de saldo");
-           
-            boolean exec = true;
-            double result;
-            int result1;
       
-            while (exec) {
 
-                int key = in.nextInt();
+            int key = Integer.parseInt(in.nextLine());
                 switch (key) {
                     case 1:
                     System.out.println("Digite sua id");
-                    int idS=in.nextInt();
+                    int idS=Integer.parseInt(in.nextLine());
+                    if(c.conta(idS)==false){
+                        throw new ILLEGAL_ARGUMENT_EXCEPTION("CONTA INEXISTENTE");
+                    }
                     System.out.println("Digite o valor a ser sacado");
-                    double valorSaque=in.nextDouble();
+                    double valorSaque=Double.parseDouble(in.nextLine());
+                    if(valorSaque<0){
+                        throw new ILLEGAL_ARGUMENT_EXCEPTION("VALOR SACADO INVALIDO");
+                    }
+                    if(valorSaque>c.getSaldo(idS)){
+                        throw new ILLEGAL_ARGUMENT_EXCEPTION("FUNDOS INSUFICIENTES ");
+                    }
                     c.saque(idS, valorSaque);
                    
                     System.out.println("Saldo em conta pos saque: "+c.getSaldo(idS));
                         break;
                     case 2:
                     System.out.println("Digite sua id");
-                    int id=in.nextInt();
+                    int id=Integer.parseInt(in.nextLine());
+                    if(c.conta(id)==false){
+                        throw new ILLEGAL_ARGUMENT_EXCEPTION("CONTA INEXISTENTE");
+                    }
                     System.out.println("Digite o valor a ser depositado");
-                    double valorDeposito=in.nextDouble();
+                    double valorDeposito=Double.parseDouble(in.nextLine());
+                    if(valorDeposito<0){
+                        throw new ILLEGAL_ARGUMENT_EXCEPTION("VALOR INSERIDO INVALIDO");
+                    }
+                
                     c.deposito(id, valorDeposito);
-                    
                     System.out.println("Saldo em conta pos saque: "+c.getSaldo(id));
                         break;
                     case 3:
-                    System.out.println("Digite seu id para identificarmos sua conta");
-                    System.out.printf("Result: %.2f\n",c.getSaldo(in.nextInt()) );
-                        break;
-                   
+                    System.out.printf("Digite sua id\n");
+                    int idG=Integer.parseInt(in.nextLine());
+                    System.out.println(c.nome(idG)+":"+ c.getSaldo(idG)+":"+c.id(idG));
+                    break;
                     case 0:
-                        exec = false;
+                    escolha=0;
+                    System.out.println("encerrando");
                     default:
+                    System.out.println("Digitou opcao invalida");
                         break;
                 }
+                System.out.println("Deseja novamente realizar a operacao? digite[1] para continuar");
+                escolha=Integer.parseInt(in.nextLine());
+                if(escolha!=1){
+                 System.out.println("encerrando");
+                }
+                
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        } 
     }
-}
